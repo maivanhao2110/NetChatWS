91d94c3fa5cd1a318b3eac8b7650f7c20a62568a
maivanhao2110 <maivanhao2110@gmail.com>
2025-10-29 22:02:31 +0700

    sửa lại 2 file bat để nhấn vào có thể chạy luôn

--- run-client.bat
+++ run-client.bat
@@ -1,6 +1,11 @@
 @echo off
-echo === Biên dịch Client ===
-javac -cp ".;lib\Java-WebSocket-1.5.6.jar" CLIENT\*.java
-echo === Chạy Client ===
-java -cp ".;lib\Java-WebSocket-1.5.6.jar;CLIENT" ChatClientGUI
+echo === Bien dich CLIENT ===
+javac -cp ".;lib\Java-WebSocket-1.5.6.jar;lib\slf4j-api-1.7.36.jar;lib\slf4j-simple-1.7.36.jar" CLIENT\*.java
+if %errorlevel% neq 0 (
+    echo Loi khi bien dich CLIENT!
+    pause
+    exit /b
+)
+echo === Chay CLIENT ===
+java -cp ".;lib\Java-WebSocket-1.5.6.jar;lib\slf4j-api-1.7.36.jar;lib\slf4j-simple-1.7.36.jar;CLIENT" ChatClientGUI
 pause
--- run-server.bat
+++ run-server.bat
@@ -1,6 +1,11 @@
 @echo off
-echo === Biên dịch Server ===
-javac -cp ".;lib\Java-WebSocket-1.5.6.jar" SERVER\*.java
-echo === Chạy Server ===
-java -cp ".;lib\Java-WebSocket-1.5.6.jar;SERVER" ChatServerGUI
+echo === Bien dich SERVER ===
+javac -cp ".;lib\Java-WebSocket-1.5.6.jar;lib\slf4j-api-1.7.36.jar;lib\slf4j-simple-1.7.36.jar" SERVER\*.java
+if %errorlevel% neq 0 (
+    echo Loi khi bien dich SERVER!
+    pause
+    exit /b
+)
+echo === Chay SERVER ===
+java -cp ".;lib\Java-WebSocket-1.5.6.jar;lib\slf4j-api-1.7.36.jar;lib\slf4j-simple-1.7.36.jar;SERVER" ChatServerGUI
 pause
